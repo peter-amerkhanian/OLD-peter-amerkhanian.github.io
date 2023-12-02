9c003c7d9f24d5b06d038be62b48f36aeca3402b
peter-amerkhanian <peter-amerkhanian@users.noreply.github.com>
2023-12-02 22:21:38 +0000

    deploy: 5dd57964edfc60c2d447bb370a7137b69353e7d2

---
+++ post/12-1-23-iterated-expectations/converter.py
@@ -0,0 +1,73 @@
+# Import necessary libraries
+import os
+import pandas as pd
+import re
+import warnings
+from pandas.core.common import SettingWithCopyWarning
+
+warnings.simplefilter(action="ignore", category=SettingWithCopyWarning)
+
+
+filename = 'IteratedExp.ipynb'
+
+# Convert a Jupyter Notebook to Markdown using os.system
+os.system(f"jupyter nbconvert --to markdown {filename}")
+
+filename_out = filename.split(".")[0] + ".md"
+
+# Read the generated Markdown file into a list of lines
+with open(filename_out, "r", encoding='utf-8') as f:
+    markdown = f.readlines()
+
+# Create a Pandas DataFrame from the list of Markdown lines
+markdown = pd.DataFrame(markdown)
+markdown.columns = ['text']
+
+# Create a boolean column 'content' to mark non-empty lines
+markdown['content'] = (
+    ~markdown['text'].str.contains(r'^\s*$', regex=True)
+)
+
+# Reset the index of the DataFrame
+markdown = markdown.reset_index()
+
+# Create a new DataFrame 'content_df' containing only non-empty lines
+content_df = markdown[markdown['content']].reset_index(drop=True)
+content = content_df['text'].values
+
+# Iterate through the content to find specific patterns and swap lines
+in_eq = False
+for i in range(len(content)):
+    # Fix Latex equations
+    if "\\begin{align*}" in content[i] or "$$" in content[i]:
+        in_eq = True
+    if ("\\end{align*}" in content[i] or "$$" in content[i]) and in_eq:
+        in_eq = False
+    if in_eq:
+        print(markdown['text'][content_df['index'][i]])
+        markdown['text'][content_df['index'][i]] = re.sub(
+            r"([^\\])\\\\$", r"\1 \\\\\\", content[i]
+            )
+        markdown['text'][content_df['index'][i]] = re.sub(
+            r"\\sum_", r"\\sum\\_", markdown['text'][content_df['index'][i]]
+            )
+        print(markdown['text'][content_df['index'][i]])
+    # Fix images that come after hidden cells
+    elif "{{< /details >}}" in content[i] and "![png]" in content[i-1]:
+        print(content[i-1], content[i])
+        print(content_df['index'][i-1], content_df['index'][i])
+        
+        # Swap the lines in the original DataFrame
+        markdown['text'][content_df['index'][i-1]] = content[i]
+        markdown['text'][content_df['index'][i]] = content[i-1]
+    else:
+        pass
+
+# Join the modified text back into a single string
+new_text = "".join(markdown['text'])
+
+# Write the modified content back to the original Markdown file
+with open("index.md", "w", encoding='utf-8') as f:
+    markdown = f.write(new_text)
+
+os.remove(filename_out)
